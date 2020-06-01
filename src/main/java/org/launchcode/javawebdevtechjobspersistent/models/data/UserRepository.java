2b1747d508f3036d08e1b4337921f25fc55ed802
emwaipopo <emmanuel_mwaipopo@cable.comcast.com>
2020-06-01 13:25:01 -0400

    added user model

---
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/UserRepository.java
@@ -0,0 +1,12 @@
+package org.launchcode.javawebdevtechjobspersistent.models.data;
+
+import org.launchcode.javawebdevtechjobspersistent.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
